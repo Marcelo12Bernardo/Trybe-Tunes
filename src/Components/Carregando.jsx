07547e46f130b92e01e1cc7be3d831a4d1b7cbef
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2023-01-29 19:02:59 -0300

    Requisito 02

---
+++ src/Components/Carregando.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+class Carregando extends Component {
+  render() {
+    return (
+      <h1>
+        Carregando...
+      </h1>
+    );
+  }
+}
+export default Carregando;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Carregando from '../Components/Carregando';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  state = {
+    name: '',
+    loading: false,
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmitBtn = (user) => {
+    const { history } = this.props;
+    this.setState({ loading: true }, async () => {
+      if (user) {
+        await createUser({ name: user });
+        return history.push('/search');
+      }
+    });
+  };
+
   render() {
+    const { name, loading } = this.state;
+    const minCharacter = 3;
+
     return (
-      <div data-testid="page-login" id="page-login">
-        <h3>Page Login</h3>
+      <div data-testid="page-login">
+        <input
+          type="text"
+          data-testid="login-name-input"
+          name="name"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ name.length < minCharacter }
+          onClick={ () => this.handleSubmitBtn(name) }
+        >
+          Entrar
+        </button>
+        { loading && <Carregando /> }
       </div>
     );
   }
 }
 
 export default Login;
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
